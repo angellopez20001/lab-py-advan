49bc8e3eb62ae73e1a258fbfe48ae99c38c175af
Angelillo <angellopez20001@gmail.com>
2025-12-17 20:52:45 +0100

    Lab Python Parallel: 1a parte

---
+++ submit_Primes_ampere_alumno13.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -p hpc-bio-ampere
+#SBATCH --chdir=/home/alumno13/lab5
+#SBATCH -J Apartado3.2
+#SBATCH --output=%u-python-advan.out
+#SBATCH --cpus-per-task=6
+#Cargamos el modulo y lo descargamos y entre ellos ponemos las ordenes necesarias con ese modulo
+module load anaconda/2025.06
+
+echo Ejecutando el notebook con $1 elementos
+for CORES in 1 2 4 6; do
+export OMP_NUM_THREADS=$CORES
+
+time(
+ipython primes-par-alumno13.ipynb $1 $CORES
+)
+done
+
+echo Ejecutando el notebook con $2 elementos
+for CORES in 1 2 4 6; do
+export OMP_NUM_THREADS=$CORES
+
+time(
+ipython primes-par-alumno13.ipynb $2 $CORES
+)
+done
+module unload anaconda/2025.06
+
+
